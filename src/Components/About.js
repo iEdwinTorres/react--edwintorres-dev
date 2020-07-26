d5a53a3543de161de19482cca440cea9a4a3e920
Edwin Torres <edwin_torres@icloud.com>
2020-07-26 07:58:23 -0400

    Added PDF and various other small changes.

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import pdf from './Documents/Resume-Aug2020.pdf';
 
 class About extends Component {
     render() {
@@ -48,7 +49,7 @@ class About extends Component {
                             </div>
                             <div className="columns download">
                                 <p>
-                                    <a href={resumeDownload} className="button">
+                                    <a href={pdf} className="button">
                                         <i className="fa fa-download"></i>
                                         Download Resume
                                     </a>
