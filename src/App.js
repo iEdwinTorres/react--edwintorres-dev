c4021ef25e696d3ed4211f07a41bc48dc8179021
Edwin Torres <edwin_torres@icloud.com>
2020-11-17 12:27:34 -0500

    Component orginization.

--- src/App.js
+++ src/App.js
@@ -5,10 +5,10 @@ import './App.css';
 import Header from './Components/Header';
 import Footer from './Components/Footer';
 import About from './Components/About';
+import Portfolio from './Components/Portfolio';
 import Resume from './Components/Resume';
 // import Contact from './Components/Contact';
 // import Testimonials from './Components/Testimonials';
-import Portfolio from './Components/Portfolio';
 
 class App extends Component {
     constructor(props) {
