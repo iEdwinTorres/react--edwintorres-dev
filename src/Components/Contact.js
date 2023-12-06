4e811b77915ebb015861187a48020141fe7b731d
Edwin Torres <me@edwintorres.dev>
2023-12-06 14:13:53 -0500

    Update Contact.js

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -7,8 +7,8 @@ class Contact extends Component {
       var city = this.props.data.address.city;
       var state = this.props.data.address.state;
       var zip = this.props.data.address.zip;
-      var phone = this.props.data.phone;
-      //var email = this.props.data.email;
+      //var phone = this.props.data.phone;
+      var email = this.props.data.email;
       var message = this.props.data.contactmessage;
     }
 
